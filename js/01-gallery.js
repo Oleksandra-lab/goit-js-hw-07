a1de90113b1307be5787c367b93b92592a70e1fc
Oleksandra-lab <tka4olex@gmail.com>
2023-07-22 09:57:39 +0300

    Galler markup is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup)
+
+function createGalleryMarkup(galleryItems){
+    return galleryItems
+    .map(
+        ({original, preview, description}) => 
+        `<li><a href=""><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></li>`
+        )
+    .join("")
+
+}
+
 
 console.log(galleryItems);
