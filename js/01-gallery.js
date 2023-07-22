8cca5f16e8c227e202822abcf9eb87e492ae5c4a
Oleksandra-lab <tka4olex@gmail.com>
2023-07-23 00:20:46 +0300

    Task 2 is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup)
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
 function createGalleryMarkup(galleryItems){
     return galleryItems
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryMarkup(galleryItems){
+    return galleryItems
+    .map(
+        ({original, preview, description}) => 
+        `<li><a href="${original}"><img class="gallery__image" src="${preview}"  alt="${description}"></a></li>`
+        )
+    .join("")
+
+}
+
+const lightbox = new SimpleLightbox('.gallery a',{ 
+    captionsData: "alt",
+    captionDelay: 250,
+    
+});
 
 console.log(galleryItems);
